a7c6fca121eeec85400f8f8573dffe6260ce825a
acord942 <acord942@10.120.148.115>
2016-12-07 15:56:32 -0500

    Changed the code in the DBConnection class to the correct code.

--- src/DBAccessClasses/DBConnection.java
+++ src/DBAccessClasses/DBConnection.java
@@ -1,35 +1,19 @@
 package DBAccessClasses;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.DriverManager;
-/**
- * this class is the one that grants database access to the entire program. Can be removed/changed if needed.
- * @author gchar158
- *
- */
+import java.sql.*;
 public class DBConnection {
-private static Connection conn=null;
-private static String dbString= "jdbc:mysql://localhost:3306/";//missing database name
-private static String username= "   ";
-private static String password= "    ";
-
-public static void initialize() throws ClassNotFoundException{
-	Class.forName("com.mysql.jdbc.Driver");
+	public static void main(String[]args){ 
+Connection conn=null; 
+try{
+	Class.forName("com.mysql.jdbc.Driver").newInstance();
+	conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/book?autoReconnect=true&useSSL=false", "root", "ganam");
+	
+}catch(Exception e){
+	e.printStackTrace();
 }
-public static Connection getMyConnection()throws SQLException{
-	if(conn==null)
-		conn=DriverManager.getConnection(dbString, username, password);
-	return conn;
+if(conn != null){
+	System.out.print("connected");
 }
-
-public static void close(){
-	if(conn !=null)
-		try{
-			conn.close();
-		}catch (SQLException ex){
-			ex.printStackTrace();
-		}
 }
 }
